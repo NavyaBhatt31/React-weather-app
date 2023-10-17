5cb86707c41efbf635319b7adbc814123e22cb62
Navya <navya.bhatt31@gmail.com>
2023-10-17 17:35:05 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Import necessary dependencies
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.css'; // Import your CSS file
 import RegistrationForm from './RegistrationForm';
 import WeatherDisplay from './WeatherDisplay';
 import { getWeatherData } from './data/weatherapi';
@@ -20,7 +20,7 @@ function App() {
   const fetchRandomBackground = () => {
     const numImagesAvailable = 1000; // Adjust this number based on the available images on Unsplash
     const randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
-    
+
     fetch(`https://source.unsplash.com/collection/your-collection-id/?sig=${randomImageIndex}`)
       .then((response) => {
         setBackgroundImage(response.url);
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,45 +1,42 @@
+// RegistrationForm.js
+
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 const RegistrationForm = ({ onSubmit }) => {
-  const navigate = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate();
 
   const onSubmitHandler = (data) => {
-    // Call the onSubmit prop to handle the registration data in the parent component
     onSubmit(data);
-
-    // Redirect to the weather page
     navigate('/weather');
   };
 
   return (
-    <div className="App">
+    <form onSubmit={handleSubmit(onSubmitHandler)} className="form">
       <h2>Registration Form</h2>
 
-      <form onSubmit={handleSubmit(onSubmitHandler)}>
-        <label>Name:</label>
-        <input type="text" {...register('name', { required: true })} />
-        {errors.name && <p className="error">Name is required</p>}
+      <label>Name:</label>
+      <input type="text" {...register('name', { required: true })} />
+      {errors.name && <p className="error">Name is required</p>}
 
-        <label>Gender:</label>
-        <select {...register('gender', { required: true })}>
-          <option value="">Select Gender</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="others">Others</option>
-        </select>
-        {errors.gender && <p className="error">Please select a gender</p>}
+      <label>Gender:</label>
+      <select {...register('gender', { required: true })}>
+        <option value="">Select Gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="others">Others</option>
+      </select>
+      {errors.gender && <p className="error">Please select a gender</p>}
 
-        <label>City:</label>
-        <input type="text" {...register('city', { required: true })} />
-        {errors.city && <p className="error">City is required</p>}
+      <label>City:</label>
+      <input type="text" {...register('city', { required: true })} />
+      {errors.city && <p className="error">City is required</p>}
 
-        <button type="submit">Register</button>
-      </form>
-    </div>
+      <button type="submit">Register</button>
+    </form>
   );
 };
 
